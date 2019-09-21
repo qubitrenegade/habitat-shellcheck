6ec9a902e60f3f023f46edaf67257e141abaf5c9
qubitrenegade <qubitrenegade@gmail.com>
2019-09-20 22:36:41 -0600

    The commit that started it all...

---
+++ plan.sh
@@ -0,0 +1,54 @@
+pkg_name=shellcheck
+hkg_name=ShellCheck
+pkg_origin=qbrd
+pkg_version=0.7.0
+pkg_license=('GPL-3')
+pkg_upstream_url="http://www.shellcheck.net/"
+pkg_description="ShellCheck is a GPLv3 tool that gives warnings and suggestions for bash/sh shell scripts"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_source="https://hackage.haskell.org/package/${hkg_name}-${pkg_version}/${hkg_name}-${pkg_version}.tar.gz"
+pkg_shasum="f6e79fb34d076504176761cc8b7c3f996f8d31bed23250fb1570e32283cd7df6"
+pkg_dirname="${hkg_name}-${pkg_version}"
+
+pkg_bin_dirs=(bin)
+pkg_lib_dirs=(lib)
+
+pkg_deps=(
+  core/glibc
+  core/gmp
+  core/libffi
+)
+
+pkg_build_deps=(
+  core/cabal-install
+  core/ghc
+)
+
+do_clean() {
+  do_default_clean
+
+  # Strip any previous cabal config
+  rm -rf /root/.cabal
+}
+
+do_build() {
+  cabal v1-sandbox init
+  cabal v1-update
+
+  # Install dependencies
+  cabal v1-install --only-dependencies
+
+  # Configure and Build
+  cabal v1-configure --prefix="$pkg_prefix" \
+    --disable-executable-dynamic \
+    --disable-shared
+  cabal v1-build
+}
+
+do_check() {
+  cabal v1-test
+}
+
+do_install() {
+  cabal v1-copy
+}
